6e23b317119e659baf42653c6e611f3c4b7d8881
Paul Oloyede <52790286+paulayo93@users.noreply.github.com>
2020-01-23 05:44:53 +0100

    Set the PORT for production on heroku

--- server/server-prod.js
+++ server/server-prod.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 const app = express(), 
             DIST_DIR = __dirname,
